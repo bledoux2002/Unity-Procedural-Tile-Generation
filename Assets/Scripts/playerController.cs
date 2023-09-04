87b5c161c92636ab20d446f5e39a6e48a6a6a15a
Benjamin Ledoux <bledoux2002@gmail.com>
2023-09-04 13:06:10 -0400

    init commit
    player spawns new tiles under self, regardless if they are there or not; initial area around (0, 0) checks for missing tiles and fills in (not centered on player/camera)

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class playerController : MonoBehaviour
+{
+    public int radiusX; //x range of tiles to check for generation
+    public int radiusY; //same but for y
+    public Tilemap map;
+    public TileBase changeTile;
+    private Vector2 input;
+    private bool isXAxisInUse = false;
+    private bool isYAxisInUse = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(transform.position);
+        //Vector2 pCoords = new Vector2(transform.position.x, transform.position.y); //curent player coords in tilemap (only int for now, player moves entire int every movement)
+        //Debug.Log(pCoords);
+        //private tileTable = ;
+        //private 
+        //Debug.Log(changeTile);
+        Debug.Log(map.GetTile(new Vector3Int (0, 1, 0))); //prints tilename
+        Debug.Log(map.GetTile(new Vector3Int (0, 2, 0))); //prints Null
+
+        for (int y = radiusY; y >= -radiusY; y--)
+        {
+            for (int x = -radiusX; x <= radiusX; x++)
+            {
+                //from top left to bottom right, generate any missing tiles (currently at (0, 0), not on player/cam
+                if (map.GetTile(new Vector3Int (x, y, 0)) == null)
+                {
+                    map.SetTile(new Vector3Int(x, y, 0), changeTile);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        if (input.x != 0) { input.y = 0; } //remove diagonal movement
+        if (input.x != 0)
+        {
+            if (isXAxisInUse == false)
+            {
+                transform.position = transform.position + new Vector3(input.x, input.y, 0);
+                isXAxisInUse = true;
+            }
+        }
+        if (input.x == 0)
+        {
+            isXAxisInUse = false;
+        }
+        if (input.y != 0)
+        {
+            if (isYAxisInUse == false)
+            {
+                transform.position = transform.position + new Vector3(input.x, input.y, 0);
+                isYAxisInUse = true;
+            }
+        }
+        if (input.y == 0)
+        {
+            isYAxisInUse = false;
+        }
+
+        Vector3Int currentCell = map.WorldToCell(transform.position);
+        
+//        for (y = )
+
+        map.SetTile(currentCell, changeTile);
+    }
+
+
+    //hash table constantly updating with loaded tiles in rendered area, values shift over as needed
+    //tile class with tile component, hashtable/variables/array of corner values
+}
