5a72d5135f09d50021551e2ab3b037c2279b7f1e
Benjamin Ledoux <bledoux2002@gmail.com>
2023-10-23 15:39:26 -0400

    level save added
    want to add a "clear level data" button, but this saves all tiles and player position

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Linq;
+using Debug = UnityEngine.Debug;
+using Application = UnityEngine.Application;
 
 public class MapManager : MonoBehaviour
 {
-
-    [SerializeField]
-    private Tilemap map;
+    public static MapManager instance;
+    public Tilemap map;
 
     [SerializeField]
     private List<TileData> tileDatas;
@@ -17,6 +21,14 @@ public class MapManager : MonoBehaviour
 
     private void Awake()
     {
+        if (instance == null) instance = this;
+        else Destroy(this);
+
+        if (File.Exists(Application.dataPath + "/levelData.json"))
+        {
+            LoadLevel();
+        }
+
         dataFromTiles = new Dictionary<TileBase, TileData>();
 
         foreach (var tileData in tileDatas)
@@ -27,10 +39,59 @@ public class MapManager : MonoBehaviour
             }
         }
     }
-
-    // Update is called once per frame
+    /*
     private void Update()
     {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.F)) SaveLevel();
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.G)) LoadLevel();
+    }*/
 
+    public void SaveLevel()
+    {
+        BoundsInt bounds = map.cellBounds;
+        LevelData levelData = new LevelData();
+        levelData.charPos = GameObject.FindWithTag("Player").transform.position;
+
+        for (int x = bounds.min.x; x <= bounds.max.x; x++)
+        {
+            for (int y = bounds.min.y; y <= bounds.max.y; y++)
+            {
+                TileBase temp = map.GetTile(new Vector3Int(x, y, 0));
+                
+                if (temp != null)
+                {
+                    //Debug.Log(x.ToString() + ", " + y.ToString());
+                    levelData.tiles.Add(temp);
+                    levelData.pos.Add(new Vector3Int(x, y, 0));
+                }
+            }
+        }
+
+        string json = JsonUtility.ToJson(levelData, true);
+        File.WriteAllText(Application.dataPath + "/levelData.json", json);
+
+        Debug.Log("Tilemap saved.");
     }
+
+    public void LoadLevel()
+    {
+        string json = File.ReadAllText(Application.dataPath + "/levelData.json");
+        LevelData data = JsonUtility.FromJson<LevelData>(json);
+
+        map.ClearAllTiles();
+
+        GameObject.FindWithTag("Player").transform.position = data.charPos;
+
+        for (int i = 0; i < data.pos.Count; i++)
+        {
+            map.SetTile(data.pos[i], data.tiles[i]);
+        }
+    }
+}
+
+public class LevelData
+{
+    public Vector3 charPos = new Vector3();
+    public List<TileBase> tiles = new List<TileBase>();
+    public List<Vector3Int> pos = new List<Vector3Int>();
 }
