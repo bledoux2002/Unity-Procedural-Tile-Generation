db800aa2382b6aa6067ccc0d7af9e64e8e1036c9
Benjamin Ledoux <bledoux2002@gmail.com>
2024-02-05 15:34:12 -0500

    soft reset on tiledata comp lists, code updates
    removed unecessary comments from TileData.cs, completed assigned tiles with directly adjacent grass (no corners, no water adjacent)

--- Assets/Scripts/TileData.cs
+++ Assets/Scripts/TileData.cs
@@ -17,11 +17,7 @@ public class TileData : ScriptableObject
     // These lists define compatible tiles on each edge (north can only be paired with tiles 1, 4, 17, while east can only be paired with tiles 3, 18, 35)
     public TileData[] north, east, south, west, northwest, northeast, southeast, southwest;
 
-    //CORNER CHECKS ARE REDUNDANT, CAN BE REMOVED BUT I'M LEAVING THEM OUT OF SPITE
-
     // Required adjacent tiles (multi-tile structures), <tile, relevant coords>
-    //Dictionary<TileBase, Vector3Int> requiredTiles = new Dictionary<TileBase, Vector3Int>();
-//    public int requiredTilesNum = 0;
-    public TileData[] reqTiles/* = new TileBase[requiredTilesNum]*/;
-    public Vector3Int[] reqTilesCoords;// = new Vector3Int() //leave z as 0
+    public TileData[] reqTiles;
+    public Vector3Int[] reqTilesCoords;
 }
